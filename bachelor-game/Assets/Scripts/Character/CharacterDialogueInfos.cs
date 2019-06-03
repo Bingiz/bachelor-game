5445e6a61db7ba8f3b5e4a526304e5daf310f81b
Jonas André <bingiz@Jonass-iMac.local>
2019-06-03 20:48:23 +0200

    Dialogue UI
    Dialogue UI added and some corresponding code

---
+++ bachelor-game/Assets/Scripts/Character/CharacterDialogueInfos.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterDialogueInfos : MonoBehaviour
+{
+    public string characterName;
+    public Color color;
+}
